590684692c33ebd7e8a51ec683cd4912ac3e28b1
Alcatraz-91 <74610574+Alcatraz-91@users.noreply.github.com>
2020-11-20 21:27:20 +0300

    Add files via upload

---
+++ KVM_Task_02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KVM_Task_02
+
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            string exit = " ";
+            Console.WriteLine("Эта программа будет работать, пока не будет введено слово exit");
+            while (exit != "exit")
+            {
+                exit = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Ты ввел: " + exit);
+            }
+        }
+    }
+}
