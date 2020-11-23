bc76df142b6a56225dfcb6d6af34376f70a8cb48
Alcatraz-91 <74610574+Alcatraz-91@users.noreply.github.com>
2020-11-23 20:42:47 +0300

    Add files via upload

---
+++ KVM_Task_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KVM_Task_03
+{
+    class Program
+    {
+        private const string password = "IP-18-4";
+        private const string message = "Друг, ты классный! Оставайся таким :)";
+        static void Main(string[] args)
+        {
+            int attempts = 0;
+            string pass = "";
+            while (!pass.Equals(password))
+            {
+                attempts++;
+                if (attempts > 3) System.Environment.Exit(1);
+                Console.WriteLine($"Enter the password. Attempt {attempts} of 3");
+                pass = Console.ReadLine();
+            }
+            Console.WriteLine($"You opened a secret message!: {message}");
+            Console.ReadKey();
+        }
+    }
+}
